c1e2a93a30cbf63ac480dfb834b539b45d1a0bfd
aid4n-e <120997312+aid4n-e@users.noreply.github.com>
2025-02-11 20:56:04 -0800

    update

---
+++ FL/FL Studio/Projects/music box/MusicBoxMaker-main/execute.py
@@ -0,0 +1,25 @@
+import musicBoxMaker
+
+prefix = ""
+suffix = ""
+#load the prefix and suffix gcode (for initialization and finish). 
+#Generate one gcode from your favorite software and copy paste the sections before and after the print of the object itself
+#Make sure to adjust the parameter start_extrusion_val for the generateGCODE function
+with open ("prefix.gcode", "r") as myfile:
+    prefix=str(myfile.read())
+with open ("suffix.gcode", "r") as myfile:
+    suffix=str(myfile.read())
+
+
+partition = musicBoxMaker.parsePartitionFile("listNotes.txt") #put your partition here
+
+musicBoxMaker.saveToSTL("fileName.stl", musicBoxMaker.generateTriangleList([114.5,95], musicBoxMaker.parsePartitionFile("listNotes.txt")))
+
+
+#adjust the parameters to adjust to your music box
+gcode = musicBoxMaker.generateGCODE(center=[114.5,95], height=20, radius=6.5, layerHeight=0.2, startZ = 1.35, endZ = 17.55, sheet=partition,start_extrusion_val=14.41425, prefix=prefix, suffix=suffix)
+#save the gcode
+with open("result_cylinder.gcode", "w") as myfile:
+    print("saving...")
+    myfile.write(gcode)
+    print("Done!!!")
--- FL/FL Studio/Projects/music box/MusicBoxMaker-main/play_partition.py
+++ FL/FL Studio/Projects/music box/MusicBoxMaker-main/play_partition.py
@@ -7,7 +7,7 @@ partition = parsePartitionFile("listNotes.txt")#put your partition here
 notes = AudioSegment.from_mp3("it.mp3")#put your recording here
 
 #put the start time (in ms) of each note here
-startTimes = [0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000, 16000, 17000]
+startTimes = [0, 1100, 2100, 3100, 4100, 5100, 6100, 7100, 8100, 9100, 10100, 11100, 12100, 13100, 14100, 15100, 16100, 17100]
 
 
 noteLength = 200 #length of each note (in ms)
